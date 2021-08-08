6a437f81dcc63648765e9917d981c6c615dea300
yomex4life <yomex4life2001@gmail.com>
2021-08-08 12:51:35 +0100

    Updated terraform and azure pipeline files

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "azurerm"{
     features {}
 }
 
+terraform{
+    backend "azurerm" {
+        resource_group_name     = "terraform_gp_blobstore"
+        storage_account_name    = "tfstorageaccount858"
+        container_name          = "tfstate"
+        key                     = "terraform.tfstate"
+    }
+}
+
 resource "azurerm_resource_group" "terraform_test" {
     name = "tfmainrg"
     location = "UK South"
