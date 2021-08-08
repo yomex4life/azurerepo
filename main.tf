7dc85c367052bdf442494f2b55e064c80daee27e
yomex4life <yomex4life2001@gmail.com>
2021-08-08 11:58:18 +0100

    added terraform file

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm"{
+    version = "2.5.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "terraform_test" {
+    name = "tfmainrg"
+    location = "UK South"
+}
+
+resource "azurerm_container_group" "tfcg_test"{
+    name                = "colorapi"
+    location            = azurerm_resource_group.terraform_test.location
+    resource_group_name = azurerm_resource_group.terraform_test.name
+    ip_address_type     = "public"
+    dns_name_label      = "yomexcolourapi"
+    os_type             = "linux"
+
+    container{
+        name            = "colourapi"
+        image           = "yomex4life/azureapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports{
+            port        = 80
+            protocol    = "TCP"
+        }
+
+    }
+}
